ad8d352c60cdadaa97955347e485d238264c72d6
rhernandez-2024310 <rhernandez-2024310@kinal.edu.gt>
2026-03-04 21:21:05 -0600

    feat: Se agregan datos para la programación de eventos especiales al igual que la lógica para asignación de recursos y servicios adicionales.

--- Restaurante/management-service/config/configuration.js
+++
@@ -1,6 +0,0 @@
-export const options = {
-    origin: true,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH'],
-    allowedHeaders:['Content-Type', 'Authorization']
-};
--- Restaurante/management-service/src/report.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getTopDishesReport, getPeakHoursReport } from "./report.controller.js";
-
-const router = Router();
-
-router.get("/top-dishes/:restaurantId", getTopDishesReport);
-router.get("/peak-hours/:restaurantId", getPeakHoursReport);
-
-export default router;
